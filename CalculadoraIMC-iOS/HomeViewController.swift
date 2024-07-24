6aa3f47a02176fc6aad5faf293522232f86f5890
Mansour <theboom.kajas@gmail.com>
2024-07-24 12:45:46 +0200

    Ya se muestra el resultado del IMC y la descripción con colores

--- CalculadoraIMC-iOS/HomeViewController.swift
+++ CalculadoraIMC-iOS/HomeViewController.swift
@@ -12,12 +12,24 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
     
+    @IBOutlet var heightStepper: UIStepper!
+    @IBOutlet var weightSlider: UISlider!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     var height: Int = 160
     var weight: Float = 70
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Do any additional setup after loading the view.
+        heightStepper.value = Double(height)
+        weightSlider.value = weight
+        
+        onHeightChanged(heightStepper)
+        onWeightChanged(weightSlider)
     }
     
     @IBAction func onHeightChanged(_ sender: UIStepper) {
@@ -27,12 +39,39 @@ class HomeViewController: UIViewController {
     
     @IBAction func onWeightChanged(_ sender: UISlider) {
         weight = sender.value
-        weightLabel.text = "\(weight) kg"
+        let formatedWeight = String(format: "%.2f", weight)
+        weightLabel.text = "\(formatedWeight) kg"
     }
     
     @IBAction func calculate(_ sender: Any) {
-        print("Altura: \(height)")
-        print("Peso: \(weight)")
+        // Calculo el IMC
+        let imc = Double(weight)/pow(Double(height)/100.0, 2)
+        let formatedIMC = String(format: "%.2f", imc)
+        
+        // Calculo el color y la descripcion
+        var descriptionText: String
+        var descriptionColor: UIColor
+        
+        switch imc {
+        case 0..<18.5:
+            descriptionText = "Bajo peso"
+            descriptionColor = UIColor(named: "under_weight")!
+        case 18.5..<25:
+            descriptionText = "Peso normal"
+            descriptionColor = UIColor(named: "normal_weight")!
+        case 25..<30:
+            descriptionText = "Sobrepeso"
+            descriptionColor = UIColor(named: "over_weight")!
+        default:
+            descriptionText = "Obesidad"
+            descriptionColor = UIColor(named: "obesity_weight")!
+        }
+        
+        // Muestro el resultado
+        resultLabel.text = "\(formatedIMC)"
+        descriptionLabel.text = descriptionText
+        resultLabel.textColor = descriptionColor
+        descriptionLabel.textColor = descriptionColor
     }
 }
 
